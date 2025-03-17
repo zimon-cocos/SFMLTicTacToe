950f66683b267f258ed583c5c61ff9823bbeec15
zimon-cocos <111566187+zimon-cocos@users.noreply.github.com>
2025-03-17 16:17:10 +0100

    Vykreslenie placeholderu v "strede" stvorca, treba ladit

--- main.cpp
+++ main.cpp
@@ -8,43 +8,63 @@ constexpr float boxWidth {49.0f};
 constexpr float boxHeight {49.0f};
 constexpr int boxAmountX {13};
 constexpr int boxAmountY {13};
+constexpr float outlineThickness{5.0f};
+
+
+
+//placeholder na sprite
+struct Sprites
+{
+    sf::CircleShape shape;
+    bool isCross {false};
+
+    Sprites(float sX, float sY)
+    {
+        shape.setOrigin(shape.getGeometricCenter());
+        shape.setPosition({sX,sY});
+        shape.setFillColor(sf::Color::Red);
+        shape.setRadius(10.0f);
+    }
+};
+
+
 
 
 struct Box
 {
-    sf::RectangleShape shape;
 
+    //!DOLEZITE!
+    sf::RectangleShape shape;
+    //!DOLEZITE!
     bool wasHit {false};
 
     void makeHit()
     {
         wasHit = true;
     }
-
-
+    float centerPosX {0};
+    float centerPosY {0};
     Box(float bX, float bY)
     {
         shape.setPosition({bX,bY});
         shape.setFillColor(sf::Color::Transparent);
         shape.setSize({boxWidth,boxHeight});
         //shape.setOrigin(shape.getGeometricCenter());
-        shape.setOutlineThickness(5.0f);
+        shape.setOutlineThickness(outlineThickness);
         shape.setOutlineColor(sf::Color::Black);
+        centerPosX = (bX + (boxWidth/2) - (outlineThickness*2));
+        centerPosY = (bY + (boxHeight/2) - (outlineThickness*2));
     }
+
     // borderBox {shape.getGlobalBounds()};
 };
 
-
-//Tvorba tvarov
-
-//Box box1{wHeight/2,wWidth/2};
-//Box box2{300,420};
 
 
 int main()
 {
     sf::RenderWindow window (sf::VideoMode({wWidth,wHeight}),"TicTacToe");
-    window.setFramerateLimit(30);
+    window.setFramerateLimit(60);
 
     std::vector<Box> boxes;
     int gapX {0};
@@ -53,7 +73,7 @@ int main()
         {
         for(int i {0};i<boxAmountX; ++i)
             {
-                boxes.emplace_back(10+gapX,gapY+10);
+                boxes.emplace_back(outlineThickness*2+gapX,outlineThickness*2+gapY);
                 gapX += 60;
             }
 
@@ -61,11 +81,7 @@ int main()
         gapY += 60;
         }
 
-
-    //boxes[0].makeHit();
-    boxes[84].makeHit();
-    boxes[168].makeHit();
-
+    std::vector<Sprites> sprites;
 
     while (window.isOpen())
     {
@@ -86,7 +102,6 @@ int main()
 
         }
 
-
     if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
         //float xClickPos  = sf::Mouse::getPosition(window).x;
         //float yClickPos = sf::Mouse::getPosition(window).y;
@@ -100,6 +115,8 @@ int main()
                 if(boxes[i].shape.getGlobalBounds().contains(transMousePos))
                 {
 
+                    sprites.emplace_back(boxes[i].centerPosX,boxes[i].centerPosY);
+                    std::cout << boxes[i].centerPosX << "," << boxes[i].centerPosY << '\n';
                     boxes[i].makeHit();
                     }
 
@@ -107,18 +124,29 @@ int main()
 
     }
 
+
     //RENDER
     window.clear(sf::Color::White);
 
     //KRESLENIE
 
     for(unsigned int i {0};i<boxes.size();++i)
-
-        if(!(boxes[i].wasHit))
         {
+            if(!(boxes[i].wasHit))
+            {
 
-            window.draw(boxes[i].shape);
+                window.draw(boxes[i].shape);
+            }
         }
+
+
+
+    for(unsigned int i {0};i<sprites.size();++i)
+        {
+            window.draw(sprites[i].shape);
+        }
+
+
 
     window.display();
 
